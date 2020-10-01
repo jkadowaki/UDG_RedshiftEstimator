ddfdc2d84af33e91dd58fc368648ee5f970f3408
jkadowaki <jkadowaki@email.arizona.edu>
2020-10-01 10:08:37 -0700

    added missing torchvision package to download pretrained model; fixed bug to print runtime duration in slurm job submission script

--- code/create_virtual_environment.sh
+++ code/create_virtual_environment.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/bash
 
-python3 -m venv smudges_env                                      # Create a new virtual environment
-source activate smudges_env/bin/activate                         # Activate virtual environment
-pip install numpy==1.19.1                                        # Install NumPy
-pip install pandas==0.25.1                                       # Install pandas
-pip install scikit-image==0.17.2                                 # Install skimage
-pip install torch==1.6.0                                         # Install PyTorch
-wget -qO- "https://yihui.org/tinytex/install-bin-unix.sh" | sh   # Installs Light Version of LaTeX
+# Create a new virtual environment
+python3 -m venv smudges_env
+
+# Activate virtual environment
+source activate smudges_env/bin/activate
+
+# Install Python Packages
+pip install numpy==1.19.1
+pip install pandas==0.25.1
+pip install scikit-image==0.17.2
+pip install torch==1.6.0
+pip install torchvision==0.2.2
+
+# Install Light Version of LaTeX
+wget -qO- "https://yihui.org/tinytex/install-bin-unix.sh" | sh
 
